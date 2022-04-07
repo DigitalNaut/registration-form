4be5229e22821f01ce3e8be0a9d4467def496953
John B <johnbernalfsd@gmail.com>
2022-04-07 18:10:09 -0500

    Revert "Fix: Resolve Netlify error"
    This reverts commit 3183f77784cec0d8dbd63f895455cdbf59ed60f2.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -11,7 +11,11 @@ export default function Gallery() {
   const [error, setError] = React.useState('');
 
   useEffect(() => {
-    // Obtener los datos con Axios
+    const requestOptions = {
+      method: 'GET',
+    }
+
+    // Obtener los datos con fetch
 
     // fetch('https://60f2262e6d44f3001778853a.mockapi.io/api/registro', requestOptions)
     //   .then(async response => {
